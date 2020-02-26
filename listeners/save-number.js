8ccbf9b94fa0479b2b14334c017380db4a9c3d7b
Oscar <oscarpete@gmail.com>
2020-02-26 05:51:05 +0100

    Listeners

--- listeners/save-number.js
+++ listeners/save-number.js
@@ -1,1 +1,1 @@
-// attach a click event listener to trigger saving new numbers
+document.getElementById('save-button').addEventListener('click', saveNumberHandler);// attach a click event listener to trigger saving new numbers
